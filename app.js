8454558d4441f3f3afe65858bd7216370ea7876d
Cheng Chi Man <ztartennery@gmail.com>
2018-07-02 11:33:59 +0800

    add win and lose

--- app.js
+++ app.js
@@ -1,63 +1,68 @@
-var hand = document.getElementById("hand"); 
+var hand = document.getElementById("hand");
 var fist = document.getElementById("fist");
 var scissors = document.getElementById("scissors");
 var number;
+var result = document.getElementById("winlose");
 
 
 
-TweenMax.set(".popup-right", {autoAlpha:0});
-TweenMax.set(".popup-left", {autoAlpha:0});
+TweenMax.set(".popup-right", { autoAlpha: 0 });
+TweenMax.set(".popup-left", { autoAlpha: 0 });
 console.log('set hidden');
 
 
-hand.addEventListener("click", function(){
-reset();
-TweenMax.to("#popup-hand-left", 1, {autoAlpha:1});
-TweenMax.from("#popup-hand-left", 1, {x:-100});
-opponent();
+hand.addEventListener("click", function() {
+    reset();
+    TweenMax.to("#popup-hand-left", 1, { autoAlpha: 1 });
+    TweenMax.from("#popup-hand-left", 1, { x: -100 });
+    opponent();
+    if (number == 2) { result.innerText = "WIN!!"; } else if (number == 3) { result.innerText = "LOSE!!"; } else { result.innerText = "STANDOFF!!"; }
 });
 
-fist.addEventListener("click", function(){
-reset();
-TweenMax.to("#popup-fist-left", 1, {autoAlpha:1});
-TweenMax.from("#popup-fist-left", 1, {x:-100});
-opponent();
+fist.addEventListener("click", function() {
+    reset();
+    TweenMax.to("#popup-fist-left", 1, { autoAlpha: 1 });
+    TweenMax.from("#popup-fist-left", 1, { x: -100 });
+    opponent();
+    if (number == 3) { result.innerText = "WIN!!"; } else if (number == 1) { result.innerText = "LOSE!!"; } else { result.innerText = "STANDOFF!!"; }
 });
 
-scissors.addEventListener("click", function(){
-reset();
-TweenMax.to("#popup-scissors-left", 1, {autoAlpha:1});
-TweenMax.from("#popup-scissors-left", 1, {x:-100});
-opponent();
-	
+scissors.addEventListener("click", function() {
+    reset();
+    TweenMax.to("#popup-scissors-left", 1, { autoAlpha: 1 });
+    TweenMax.from("#popup-scissors-left", 1, { x: -100 });
+    opponent();
+    if (number == 1) { result.innerText = "WIN!!"; } else if (number == 2) { result.innerText = "LOSE!!"; } else { result.innerText = "STANDOFF!!"; }
 });
 
 
 
 
-function reset(){
-	TweenMax.set(".popup-right", {autoAlpha:0});
-TweenMax.set(".popup-left", {autoAlpha:0});
-number = Math.floor((Math.random() * 3) + 1);
+function reset() {
+    TweenMax.set(".popup-right", { autoAlpha: 0 });
+    TweenMax.set(".popup-left", { autoAlpha: 0 });
+    number = Math.floor((Math.random() * 3) + 1);
 }
 
 
 
 
-function opponent(){
-if(number == 1){
+function opponent() {
+    if (number == 1) {
 
-TweenMax.to("#popup-hand-right", 1, {autoAlpha:1});
-TweenMax.from("#popup-hand-right", 1, {x:100});
+        TweenMax.to("#popup-hand-right", 1, { autoAlpha: 1 });
+        TweenMax.from("#popup-hand-right", 1, { x: 100 });
 
-}if(number == 2){
+    }
+    if (number == 2) {
 
-TweenMax.to("#popup-fist-right", 1, {autoAlpha:1});
-TweenMax.from("#popup-fist-right", 1, {x:100});
+        TweenMax.to("#popup-fist-right", 1, { autoAlpha: 1 });
+        TweenMax.from("#popup-fist-right", 1, { x: 100 });
 
-}if(number == 3){ 
+    }
+    if (number == 3) {
 
-TweenMax.to("#popup-scissors-right", 1, {autoAlpha:1});
-TweenMax.from("#popup-scissors-right", 1, {x:100});
-}
+        TweenMax.to("#popup-scissors-right", 1, { autoAlpha: 1 });
+        TweenMax.from("#popup-scissors-right", 1, { x: 100 });
+    }
 }
